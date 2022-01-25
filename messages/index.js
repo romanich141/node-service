e14de2f51820ab97add14e41f770cd98ce0c5495
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-01-25 09:59:31 +0200

    add messages

---
+++ messages/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'success_auth': 'Вы удачно авторизировались к',
+    'success_logout': 'Вы удачно вышли',
+    'error_auth': 'Ошибка авторизации к',
+    'wrong_auth': 'Вы не авторизированы',
+    'wrong_logout': 'Ошибка выхода',
+    'error_server': 'Internal server error'
+};
